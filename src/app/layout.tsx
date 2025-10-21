7bbe41844375bcc8dd8c1b52d900f33a88406b87
wasim md. <md.wasim0@gmail.com>
2025-10-21 10:06:05 +0000

    ekti page theke onno page e jete eto time lagche kno

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,14 @@ import { Toaster } from '@/components/ui/toaster';
 import Header from '@/components/layout/header';
 import Footer from '@/components/layout/footer';
 import { AuthProvider } from '@/context/auth-context';
+import { PT_Sans } from 'next/font/google';
+import { cn } from '@/lib/utils';
+
+const ptSans = PT_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-pt-sans',
+});
 
 export const metadata: Metadata = {
   title: 'Shohoz Yatra',
@@ -17,12 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased flex flex-col min-h-screen">
+      <body
+        className={cn(
+          "font-body antialiased flex flex-col min-h-screen",
+          ptSans.variable
+        )}
+      >
         <AuthProvider>
           <Header />
           <main className="flex-grow">{children}</main>
